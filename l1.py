4318587f5e60083b6e9aeda5bb6205e3dbc90a00
leah-is-offline <copeland@ualberta.ca>
2021-01-19 16:55:34 -0800

    print out own source code

--- l1.py
+++ l1.py
@@ -1,3 +1,7 @@
 import requests
+import json
 
-print(requests.__version__) #print version of requests 
+print(requests.__version__) #print version of requests
+
+response = requests.get("https://www.google.ca", timeout = 30)#get google homepage
+#print(response.status_code)
