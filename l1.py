9c96fdfce9d4d22f4f9fd7bb6d07aae0a2ea5321
Leah Copeland <copeland@ualberta.ca>
2021-01-20 14:10:37 -0800

    Update l1.py
    changed raw url to reflect repository name change

--- l1.py
+++ l1.py
@@ -6,6 +6,6 @@ print(requests.__version__) #print version of requests
 response = requests.get("https://www.google.ca", timeout = 30)#get google homepage
 print(response.status_code)
 
-url = "https://raw.githubusercontent.com/leah-is-offline/Web-Applications-and-Architect/master/l1.py"
+url = "https://raw.githubusercontent.com/leah-is-offline/CMPUT404LAB1/master/l1.py"
 response = requests.get(url)
 print(response.text)
